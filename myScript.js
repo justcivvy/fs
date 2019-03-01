fd8817bfec54d2c2eed06194168eeb47a754f33b
civvy <civvy.ornellas@gmail.com>
2019-03-01 17:10:04 -0500

    dsadsa

--- myScript.js
+++ myScript.js
@@ -1,9 +1,12 @@
 
 $(document).ready(function (callMeIshmael) {
+
   jQuery.get("https://awesome-page-fullstory.glitch.me/sentences.txt",
   function(whiteWhale) {
-    //var harpoon = whiteWhale;
-      document.getElementById("demo").innerHTML = whiteWhale;
+
+    document.getElementById("demo").innerHTML = whiteWhale;
+
+  
 
   });
 
