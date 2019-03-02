e2c22273e29ec4aa47a50d0410aef7c5ada1d6ea
civvy <civvy.ornellas@gmail.com>
2019-03-02 12:13:42 -0500

    getting the correct vowel count for each word

--- myScript.js
+++ myScript.js
@@ -1,10 +1,10 @@
 
 var fs = require('fs');
-var vowels = 'aeiouAEIOU';
-var i = 0;
+var vowel_list = 'aeiouAEIOU';
+var i=0;
 
 
-fs.readFile('sentences.txt', function(err, text) {
+fs.readFile('sentences.txt', function(err, text) { //get the text or error
   if(err) throw err;
 
   var lines = text.toString().split("\n"); //put the text into an array of lines
@@ -13,40 +13,30 @@ fs.readFile('sentences.txt', function(err, text) {
     var words = line.toString().split(" "); //put each line into an array of words
 
     words.forEach(function(word) {
-      var vcount = 0;
-
-
-        if (vowels.indexOf(word[i]) !== -1) {
-          vcount += 1;
-
+      function vowel_count(word) {
+        var vcount = 0;
+        for(var x = 0; x < word.length ; x++) {
+          if (vowel_list.indexOf(word[x]) !== -1) {
+            vcount += 1;
+          }
         }
-        console.log(vcount);
+        return vcount;
+      }
+      console.log(vowel_count(word));
+
     });
 
     console.log('Words' + '['+ i +']' + ', Length: ' + words.length + ' ' + words);
     i++;
+
   });
+
 });
 
 
 
 
-function vowel_count(str1)
-{
-  var vowel_list = 'aeiouAEIOU';
-  var vcount = 0;
 
-  for(var x = 0; x < str1.length ; x++)
-  {
-    if (vowel_list.indexOf(str1[x]) !== -1)
-    {
-      vcount += 1;
-    }
-
-  }
-  return vcount;
-}
-//console.log(vowel_count("The quick brown fox"));
 
 
   //    for(i in lines) {
