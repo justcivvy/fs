3fbe44afa4370f1bd86bfc4d33a51d75277dd072
civvy <civvy.ornellas@gmail.com>
2019-03-02 13:44:38 -0500

    logging the word alongside the vowel count

--- myScript.js
+++ myScript.js
@@ -1,8 +1,9 @@
 
 var fs = require('fs');
-var vowel_list = 'aeiouAEIOU';
-var i=0;
-
+var vowels = 'aeiouAEIOU';
+var i = 0;
+var x = 0;
+var vcount = 0;
 
 fs.readFile('sentences.txt', function(err, text) { //get the text or error
   if(err) throw err;
@@ -16,43 +17,24 @@ fs.readFile('sentences.txt', function(err, text) { //get the text or error
       function vowel_count(word) {
         var vcount = 0;
         for(var x = 0; x < word.length ; x++) {
-          if (vowel_list.indexOf(word[x]) !== -1) {
+          if (vowels.indexOf(word[x]) !== -1) {
             vcount += 1;
+
           }
+
         }
+
         return vcount;
       }
-      console.log(vowel_count(word));
+      if (vcount < 2) {
+      console.log(vowel_count(word) + word);
 
+      }
     });
 
-    console.log('Words' + '['+ i +']' + ', Length: ' + words.length + ' ' + words);
+  // console.log('Words' + '['+ i +']' + ', Length: ' + words.length + ' ' + words);
     i++;
 
   });
 
 });
-
-
-
-
-
-
-
-  //    for(i in lines) {
-//        var words = lines[i].toString().split("\n");
-//        console.log(words[i])
-  //  }
-
-
-
-
-//var array1 = ['a', 'b', 'c'];
-
-//array1.forEach(function(element) {
-  //console.log(element);
-//});
-
-// expected output: "a"
-// expected output: "b"
-// expected output: "c"
