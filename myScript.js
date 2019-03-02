2b1b9e7d100b834525f08fe1e273a6d68367acf8
civvy <civvy.ornellas@gmail.com>
2019-03-02 18:13:57 -0500

    v3

--- myScript.js
+++ myScript.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var vowels = 'aeiouAEIOU';
 var x = 0;
 var vcount = 0;
-var y = 0;
+var wcount = 0;
+
 
 fs.readFile('sentences.txt', function(err, text) { //get the text or error
   if(err) throw err;
@@ -14,21 +15,23 @@ fs.readFile('sentences.txt', function(err, text) { //get the text or error
     var words = line.toString().split(" "); //put each line into an array of words
 
     words.forEach(function(word) {
-      y += 1;
-      function vowel_count(word) {
-        var vcount = 0;
+      var vcount = 0;
         for(var x = 0; x < word.length ; x++) {
           if (vowels.indexOf(word[x]) !== -1) {
             vcount += 1;
+
           }
+
         }
+        if (vcount >= 2) {
+          wcount += 1;
+          console.log(word);
+          console.log(wcount);
+       }
         return vcount;
-      }
-      if (vowel_count(word) > 2) {
-        console.log(vowel_count(word) + word);
-      }
+
     });
     //console.log(words);
+    //console.log('Length: ' + words.length + ' ' + words);
   });
-
 });
