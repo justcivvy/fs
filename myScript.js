bd21aeeba9b94c3f6bac31db87433f649be89e78
civvy <civvy.ornellas@gmail.com>
2019-03-02 14:11:43 -0500

    getting just the words with > 2 vowels

--- myScript.js
+++ myScript.js
@@ -1,9 +1,9 @@
 
 var fs = require('fs');
 var vowels = 'aeiouAEIOU';
-var i = 0;
 var x = 0;
 var vcount = 0;
+var y = 0;
 
 fs.readFile('sentences.txt', function(err, text) { //get the text or error
   if(err) throw err;
@@ -14,26 +14,21 @@ fs.readFile('sentences.txt', function(err, text) { //get the text or error
     var words = line.toString().split(" "); //put each line into an array of words
 
     words.forEach(function(word) {
+      y += 1;
       function vowel_count(word) {
         var vcount = 0;
         for(var x = 0; x < word.length ; x++) {
           if (vowels.indexOf(word[x]) !== -1) {
             vcount += 1;
-
           }
-
         }
-
         return vcount;
       }
-      if (vcount < 2) {
-      console.log(vowel_count(word) + word);
-
+      if (vowel_count(word) > 2) {
+        console.log(vowel_count(word) + word);
       }
     });
-
-    i++;
-
+    //console.log(words);
   });
 
 });
