e8c0e7fe6cef0e8173c6085043af9c5b70fbe6b4
civvy <civvy.ornellas@gmail.com>
2019-03-03 12:31:36 -0500

    final

--- myScript.js
+++ myScript.js
@@ -1,45 +1,43 @@
 
-var fs = require('fs');
+const fs = require('fs');
 
 fs.readFile('sentences.txt', function(err, text) { //get the text or error
   if(err) throw err;
 else
+  console.log('There are ' + splitText(text) + ' lines that have at least seven' +
+  ' with greater than or equal to two vowels.');
 
-console.log('There are ' + splitText(text) + ' lines that have at least 7 words with 2 vowels.');
-
-function splitText(text) {
-  var lcount = 0;
-  var lines = text.toString().split("\n");
-    lines.forEach(function(line) {
-      if (splitLines(line) >= 7) {
-        lcount += 1;
-      }
-    });
-    //console.log('lcount' + lcount);
+  function splitText(text) {
+    var lcount = 0;
+    var lines = text.toString().split("\n");
+      lines.forEach(function(line) {
+        if (splitLines(line) >= 7) {
+          lcount += 1;
+        }
+      });
     return lcount;
-}
+  }
 
-function splitLines(line) {
-  var wcount = 0;
-  var words = line.toString().split(" ");
-  words.forEach(function(word) {
+  function splitLines(line) {
+    var wcount = 0;
+    var words = line.toString().split(" ");
+    words.forEach(function(word) {
       if (countVowels(word) >= 2) {
         wcount++;
       }
- });
- //console.log('wcount' + wcount);
- return wcount;
-}
-
-function countVowels(word) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-  var vcount = 0;
-  for (var x = 0; x < word.length ; x++) {
-    if (vowels.includes(word[x].toLowerCase())) {
-      vcount++;
-    }
+    });
+   return wcount;
   }
-  //console.log(word + vcount);
-  return vcount;
-}
+
+  function countVowels(word) {
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    var vcount = 0;
+    for (var x = 0; x < word.length ; x++) {
+      if (vowels.includes(word[x].toLowerCase())) {
+        vcount++;
+      }
+    }
+    return vcount;
+  }
+
 });
