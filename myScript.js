32c4427f9c5417e20e122c750c9bdd776f24c8eb
civvy <civvy.ornellas@gmail.com>
2019-03-03 11:39:33 -0500

    removed a bunch of comments

--- myScript.js
+++ myScript.js
@@ -8,49 +8,38 @@ else
 splitText(text);
 
 function splitText(text) {
-  //console.log('start lines');
   var lcount = 0;
   var lines = text.toString().split("\n");
     lines.forEach(function(line) {
       if (splitLines(line) >= 7) {
         lcount++;
       }
-      console.log(lcount);
+      console.log('lcount' + lcount);
       return lcount;
     });
 }
 
 function splitLines(line) {
-  //console.log(typeof line);
-  var wcount = 0;
   var words = line.toString().split(" ");
   words.forEach(function(word) {
-    //countVowels(word);
-    //console.log(countVowels(word));
-  if (countVowels(word) >= 2) {
-     wcount++;
-     //console.log('got to wcount');
-   }
-   //console.log(wcount);
+    var wcount = 0;
+      if (countVowels(word) >= 2) {
+        wcount++;
+      }
+   console.log('wcount' + wcount);
    return wcount;
-  });
+ });
 }
 
 function countVowels(word) {
-  //console.log(word);
   var vowels = ['a', 'e', 'i', 'o', 'u'];
   var vcount = 0;
-  //console.log(word + word.length);
-  //console.log(typeof word);
   for (var x = 0; x < word.length ; x++) {
-    //console.log(word[x]);
     if (vowels.includes(word[x].toLowerCase())) {
-      //console.log('enter');
       vcount++;
     }
   }
-  //console.log(word + vcount);
+  console.log(word + vcount);
   return vcount;
 }
-
 });
